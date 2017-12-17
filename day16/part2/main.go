67eea0056a1cd8e70900e7ce8c2628397df25780
Hee Jun Yi <junyi.hjy@gmail.com>
2017-12-17 11:00:53 +0800

    Add solution for day 16

---
+++ day16/part2/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+var path = "input.txt"
+
+type Program struct {
+	bytes     []byte
+	positions map[byte]int
+	length    int
+	offset    int
+}
+
+func newProgram(length int) Program {
+	b := make([]byte, length)
+	p := make(map[byte]int)
+	for i := 0; i < length; i++ {
+		b[i] = byte(i)
+		p[b[i]] = i
+	}
+	return Program{
+		bytes:     b,
+		positions: p,
+		length:    length,
+		offset:    0,
+	}
+}
+
+func (program *Program) Print() string {
+	l := program.length
+	tempBytes := make([]byte, l)
+	for i := 0; i < l; i++ {
+		tempBytes[i] = 'a' + program.bytes[(i-program.offset+l)%l]
+	}
+	return string(tempBytes)
+}
+
+func (program *Program) From(input string) {
+	bytes := []byte(input)
+	for i := 0; i < program.length; i++ {
+		b := bytes[i] - 'a'
+		program.positions[b] = i
+		program.bytes[i] = b
+	}
+	program.offset = 0
+}
+
+func (program *Program) Swap(pos1 int, pos2 int) {
+	temp := program.bytes[pos1]
+	program.bytes[pos1] = program.bytes[pos2]
+	program.bytes[pos2] = temp
+
+	program.positions[program.bytes[pos1]] = pos1
+	program.positions[program.bytes[pos2]] = pos2
+}
+
+func (program *Program) Move(input string) {
+	switch input[0] {
+	case 's':
+		offset, _ := strconv.Atoi(input[1:])
+		program.offset = (program.offset + offset) % program.length
+	case 'x':
+		l := program.length
+		arr := strings.Split(input[1:], "/")
+		pos1, _ := strconv.Atoi(arr[0])
+		pos2, _ := strconv.Atoi(arr[1])
+		epos1 := (pos1 - program.offset + l) % l
+		epos2 := (pos2 - program.offset + l) % l
+		program.Swap(epos1, epos2)
+	case 'p':
+		arr := strings.Split(input[1:], "/")
+		pos1 := program.positions[arr[0][0]-'a']
+		pos2 := program.positions[arr[1][0]-'a']
+		program.Swap(pos1, pos2)
+	}
+}
+
+func main() {
+	b, _ := ioutil.ReadFile(path)
+	str := string(b)
+	arr := strings.Split(strings.Trim(str, "\n"), ",")
+
+	cache := make(map[string]string)
+	program := newProgram(16)
+
+	var result string
+	// Don't need to run exactly 1 billion times
+	// since the result converges to a single value
+	for i := 0; i < 1000*1000; i++ {
+		if val, ok := cache[result]; ok {
+			program.From(val)
+			result = program.Print()
+			continue
+		}
+		for _, move := range arr {
+			program.Move(move)
+		}
+		newResult := program.Print()
+		cache[result] = newResult
+		result = newResult
+	}
+
+	fmt.Println("Result =", program.Print())
+}
